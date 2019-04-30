fcfcb76b6d7984c5573226b12979ede783ac59d3
period4b <period4b@cosme.guhsd.gtec>
2019-04-30 12:14:02 -0700

    added comment to connect and undid descending order

--- resources/backend/content/connect.php
+++ resources/backend/content/connect.php
@@ -1,15 +1,18 @@
+<!-- This is the class for the databse -->
+<!-- This file never changes unless you need to update the dabatase -->
+
 <?php
-	class DatabaseContent
+	class DatabaseContent //Defining the class
 	{
-    private static $dbName = 'benrud_SnapShotPosts' ;
-    private static $dbHost = 'localhost' ;
-    private static $dbUsername = 'benrud_cosmeC';
-    private static $dbUserPassword = 'SnapshotBackEndPost42069';
+    private static $dbName = 'benrud_SnapShotPosts' ; //This is the name of the database
+    private static $dbHost = 'localhost' ; //The host is localhost because it's hosted on the same computer
+    private static $dbUsername = 'benrud_cosmeC'; //This is the username for the database access
+    private static $dbUserPassword = 'SnapshotBackEndPost42069'; //This is the password
     
-    private static $cont  = null;
+    private static $cont  = null; //defining content
      
-    public function __construct() {
-        die('Init function is not allowed');
+    public function __construct() { //Constructing the file
+        die('Init function is not allowed'); //Constructing other things
     }
      
     public static function connect()
